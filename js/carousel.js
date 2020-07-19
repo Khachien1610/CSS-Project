f667caee496e06334ed09c6ccf53322ea4b81487
khachien1610 <khachien1610@gmail.com>
2020-07-20 06:22:41 +0700

    Fixed: carousel, done album artist and new release

--- js/carousel.js
+++ js/carousel.js
@@ -7,9 +7,5 @@ owl.owlCarousel({
     autoplayTimeout: 5000,
     autoplayHoverPause: true
 });
-$('.play').on('click', function () {
-    owl.trigger('play.owl.autoplay', [5000])
-})
-$('.stop').on('click', function () {
-    owl.trigger('stop.owl.autoplay')
-})
+
+
